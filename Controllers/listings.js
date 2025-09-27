9f01c66e64fad1a4a7570ba4c28346474f00840a
Anuja Shelke <shelkeanuja155@gmail.com>
2025-09-27 20:28:41 +0530

    added coordinates and a  marker to the map

--- Controllers/listings.js
+++ Controllers/listings.js
@@ -1,106 +1,145 @@
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
+// Show all listings
 module.exports.index = async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", { allListings });
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
 };
 
+// Render new listing form
 module.exports.renderNewForm = (req, res) => {
-    res.render("./listings/new.ejs");
+  res.render("./listings/new.ejs");
 };
 
+// Show individual listing
 module.exports.showListing = async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate({path: "reviews", populate: {path: "author"}, }).populate("owner");
-    if(!listing) {
-        req.flash("error", "Listing does not exist!");
-        res.redirect("/listings");
-    }
-    console.log(listing);
-    res.render("listings/show.ejs", { listing });
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews",
+      populate: { path: "author" },
+    })
+    .populate("owner");
+
+  if (!listing) {
+    req.flash("error", "Listing does not exist!");
+    return res.redirect("/listings");
+  }
+
+  res.render("listings/show.ejs", { listing });
 };
 
+// Create new listing
 module.exports.createListing = async (req, res, next) => {
-    let url = req.file.path;      // This is the URL from Cloudinary
-    let filename = req.file.filename;  // This is the filename for Cloudinary storage
-    console.log("Cloudinary URL:", url, "Filename:", filename); 
-        if (!req.body.listing || !req.file) {
-           req.flash("error", "Missing required fields!");
-           return res.redirect("/listings/new");
-        }
-        try {
-        const newListing = new Listing({
-            ...req.body.listing, // Other listing data
-            image: {
-                url: url,       // Save the Cloudinary URL here
-                filename: filename,  // Save the filename here
-            },
-            owner: req.user._id, // Add owner
-        });
-        await newListing.save();
-        req.flash("success", "New Listing Created!");
-        res.redirect("/listings");
-        } catch (err) {
-        console.error("Error creating listing:", err);
-        req.flash("error", "Failed to create listing.");
-        res.redirect("/listings/new");
-        }
+  try {
+    if (!req.body.listing) {
+      req.flash("error", "Invalid listing data!");
+      return res.redirect("/listings/new");
+    }
+
+    // Mapbox Geocoding
+    const geoResponse = await geocodingClient
+      .forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+      })
+      .send();
+
+    if (!geoResponse.body.features.length) {
+      req.flash("error", "Invalid location! Try another one.");
+      return res.redirect("/listings/new");
+    }
+
+    const geoData = geoResponse.body.features[0].geometry;
+    console.log("Geocoding data:", geoData);
+
+    if (!req.file) {
+      req.flash("error", "Please upload an image!");
+      return res.redirect("/listings/new");
+    }
+
+    const newListing = new Listing({
+      ...req.body.listing,
+      geometry: geoData,
+      image: {
+        url: req.file.path,
+        filename: req.file.filename,
+      },
+      owner: req.user._id,
+    });
+
+    const savedListing = await newListing.save();
+    console.log(" New listing created:", savedListing);
+
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+  } catch (err) {
+    console.error(" Error creating listing:", err);
+    req.flash("error", "Failed to create listing.");
+    res.redirect("/listings/new");
+  }
 };
 
+// Render edit form
 module.exports.renderEditForm = async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing) {
-        req.flash("error", "Listing does not exist!");
-        res.redirect("/listings");
-    }
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
 
-    let originalImageUrl = listing.image.url;
-    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250,e_blur:100");
-    res.render("listings/edit.ejs", { listing, originalImageUrl });
+  if (!listing) {
+    req.flash("error", "Listing does not exist!");
+    return res.redirect("/listings");
+  }
+
+  // Generate blurred preview for existing image
+  let originalImageUrl = listing.image.url.replace(
+    "/upload",
+    "/upload/w_250,e_blur:100"
+  );
+
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
+// Update listing
 module.exports.updateListing = async (req, res) => {
-    let { id } = req.params;
-    console.log("Full Request Body:", req.body); // Log the request body to see what's inside
-    console.log("Uploaded File:", req.file); // Log the file to check if it's being uploaded
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
 
-    let listing = await Listing.findById(id);
-    if (!listing) {
-        req.flash("error", "Listing not found!");
-        return res.redirect("/listings");
-    }
-  
-    const { listing: listingData } = req.body;
-    const updatedData = {
+  if (!listing) {
+    req.flash("error", "Listing not found!");
+    return res.redirect("/listings");
+  }
+
+  const { listing: listingData } = req.body;
+
+  // Update fields
+  Object.assign(listing, {
     title: listingData.title || listing.title,
     description: listingData.description || listing.description,
     price: listingData.price || listing.price,
     location: listingData.location || listing.location,
-    country: listingData.country || listing.country
-};
-Object.assign(listing, updatedData);
+    country: listingData.country || listing.country,
+  });
 
-    // Check if an image file is uploaded
-    if (req.file) {
-        // If a file is uploaded, update the image
-        listing.image = { url: req.file.path, filename: req.file.filename };
-    } else if (!listing.image?.url && !req.body.listing.image?.url) {
-        // If no image is provided, and the listing doesn't already have an image, show error
-        req.flash("error", "Image is required!");
-        return res.redirect(`/listings/${id}/edit`);
-    }
+  // If a new image is uploaded
+  if (req.file) {
+    listing.image = { url: req.file.path, filename: req.file.filename };
+  } else if (!listing.image?.url) {
+    req.flash("error", "Image is required!");
+    return res.redirect(`/listings/${id}/edit`);
+  }
 
-    await listing.save();
-    req.flash("success", "Listing updated!");
-    res.redirect(`/listings/${id}`);
-
-    module.exports.destroyListing = async (req, res) => {
-        let { id } = req.params;
-        await Listing.findByIdAndDelete(id);
-        req.flash("success", "Listing deleted!");
-        res.redirect("/listings");
-    };
+  await listing.save();
+  req.flash("success", "Listing updated!");
+  res.redirect(`/listings/${id}`);
 };
 
-
+// Delete listing
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing deleted!");
+  res.redirect("/listings");
+};
--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,14 @@
 
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [73.8786, 18.5246], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 9 // starting zoom
+});
 
-
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)  //listing.geometry.coordinates
+    .setPopup(new mapboxgl.Popup({ offset: 25})
+        .setHTML(`<h4> ${listing.title}</h4><p>Exact location will be provided after booking</p>`)
+        .setMaxWidth("300px"))
+    .addTo(map);
